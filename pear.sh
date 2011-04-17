42f0a631e315109147b931a29c25709a7b6023ab
Arne Blankerts <Arne@Blankerts.de>
2011-04-17 22:57:26 +0200

    Pear packaging

---
+++ pear.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm -f php2odp*.tgz
+mkdir -p tmp/TheSeer/php2odp/odp
+cp src/*.php tmp/TheSeer/php2odp
+cp src/odp/*.odp tmp/TheSeer/php2odp/odp
+cp src/odp/*.xml tmp/TheSeer/php2odp/odp
+cp package.xml tmp
+cp php2odp.bat php2odp.php tmp
+cd tmp
+pear package
+mv php2odp*.tgz ..
+cd ..
+#rm -rf tmp
